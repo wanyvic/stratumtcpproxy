a8311ad16dc40595dd16cf90513d997b835263c8
Wany <q873040807@gmail.com>
2020-08-14 17:38:44 +0800

    Update channel.go

--- channel.go
+++ channel.go
@@ -1,7 +1,7 @@
 /*
  * @Date: 2020-08-14 14:44:38
  * @LastEditors: liuruihao@huobi.com
- * @LastEditTime: 2020-08-14 17:02:27
+ * @LastEditTime: 2020-08-14 17:38:22
  * @FilePath: /tcpproxy/channel.go
  */
 package main
@@ -48,25 +48,39 @@ func (c *Channel) Run() {
 }
 
 func (c *Channel) client2Server() {
+	defer c.Close()
 	scanner := bufio.NewScanner(c.inBind)
 	msg := JsonRpcRequest{}
 	for scanner.Scan() {
-		if json.Unmarshal(scanner.Bytes(), &msg) != nil {
+		b := scanner.Bytes()
+		if err := json.Unmarshal(b, &msg); err == nil {
 			if msg.Method == "mining.authorize" && len(msg.Params) > 0 {
-				c.log = logging.Logger(c.inBind.RemoteAddr().String() + string(msg.Params[0]))
+				user := ""
+				if err := json.Unmarshal(msg.Params[0], &user); err != nil {
+					c.log.Error("json Unmarshal failed: ", err)
+				} else {
+					c.log = logging.Logger(c.inBind.RemoteAddr().String() + " " + user)
+				}
 			}
+		} else {
+			c.log.Error("json Unmarshal failed: ", err)
 		}
 		c.log.Debug("s<--: ", scanner.Text())
-		if _, err := c.outBind.Write(scanner.Bytes()); err != nil {
+		b = append(b, '\n')
+		if _, err := c.outBind.Write(b); err != nil {
 			c.log.Error("write to outbind failed: ", err)
 		}
 	}
 	c.log.Info("inbind conn close")
 }
 func (c *Channel) server2Client() {
+	defer c.Close()
 	scanner := bufio.NewScanner(c.outBind)
 	for scanner.Scan() {
 		c.log.Debug("s-->: ", scanner.Text())
+		if _, err := c.inBind.Write([]byte(scanner.Text() + "\n")); err != nil {
+			c.log.Error("write to inbind failed: ", err)
+		}
 	}
 	c.log.Info("outbind conn close")
 }
